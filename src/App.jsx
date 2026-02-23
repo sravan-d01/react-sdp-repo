f1265a34977fe63995b232f9084bfc19dba42700
Sravan <dharanalakotasravan@gmail.com>
2026-02-23 12:28:06 +0530

    project v2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
-import MainNavBar from './pages/MainNavBar';
-
+import { BrowserRouter} from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './App.css';
+import MainNavBar from './pages/MainNavBar';
+import AdminNavBar from './admin/AdminNavBar';
+import ManagerNavBar from './manager/ManagerNavBar';
+import CustomerNavBar from './customer/CustomerNavBar';
 
 function App() {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isManager, setIsManager] = useState(false);
+  const [isCustomer, setIsCustomer] = useState(false);
+
+  useEffect(() => {
+    // Check sessionStorage for user role
+    const adminStatus = sessionStorage.getItem('isAdmin') === 'true';
+    const managerStatus = sessionStorage.getItem('isManager') === 'true';
+    const customerStatus = sessionStorage.getItem('isCustomer') === 'true';
+
+    setIsAdmin(adminStatus);
+    setIsManager(managerStatus);
+    setIsCustomer(customerStatus);
+  }, []);
+
   return (
-    <BrowserRouter>
-    <MainNavBar/>
-    </BrowserRouter>
+     <BrowserRouter>
+         {isAdmin && <AdminNavBar/>}
+         {isManager && <ManagerNavBar/>}
+         {isCustomer && <CustomerNavBar/>}
+         {!isAdmin && !isManager && !isCustomer && <MainNavBar/>}
+     </BrowserRouter>
   );
 }
 
